c5cae61881d1aed733d5cb4298f063669a09e046
EmilLM <luchianemil@gmail>
2021-03-02 22:41:35 +0200

    project info styling

--- components/v5/Projects/Coverflow/Project.jsx
+++ components/v5/Projects/Coverflow/Project.jsx
@@ -1,24 +1,61 @@
 import style from './project.module.scss';
+import { useState } from 'react';
 
 const Project = ({ project }) => {
-	const { title, stack, link, sourceLink } = project;
+	const { title, stack, link, sourceLink, description } = project;
+	const [showInfo, setShowInfo] = useState(false);
+
 	return (
-		<div
-			className={style.content}
-			// swiper parallax 
-			data-swiper-parallax='-30%'
-			data-swiper-parallax-scale='.6'
-			data-swiper-parallax-opacity='0'
-		>
-			<h3>{title}</h3>
-			<p>-{stack}-</p>
-			<a href={link} target='_blank' rel='noopener noreferrer'>
-				Demo
-			</a>
-			<a href={sourceLink} target='_blank' rel='noopener noreferrer'>
-				Source
-			</a>
-		</div>
+		<>
+			<div
+				className={style.button_info}
+				// swiper parallax
+				data-swiper-parallax='-30%'
+				data-swiper-parallax-scale='.6'
+				data-swiper-parallax-opacity='0'
+			>
+				{!showInfo ? (
+					<button
+						className={!showInfo ? style.pulse : ''}
+						onClick={() => setShowInfo(!showInfo)}
+					>
+						?
+					</button>
+				) : (
+					<button
+						className={style.close}
+						onClick={() => setShowInfo(!showInfo)}
+					>
+						X
+					</button>
+				)}
+			</div>
+			{showInfo ? (
+				// rework the cond render
+				<div
+					className={`${style.content } ${!showInfo?style.exit:null}`}
+					// swiper parallax
+					data-swiper-parallax='-30%'
+					data-swiper-parallax-scale='.6'
+					data-swiper-parallax-opacity='0'
+				>
+					<h3>{title}</h3>
+					<p>{description}</p>
+					<p>-{stack}-</p>
+					<div>
+						<a href={link} target='_blank' rel='noopener noreferrer'>
+							Demo
+						</a>
+						&nbsp;
+						<a href={sourceLink} target='_blank' rel='noopener noreferrer'>
+							Source
+						</a>
+					</div>
+				</div>
+			) : (
+				''
+			)}
+		</>
 	);
 };
 
